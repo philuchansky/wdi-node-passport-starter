7ac44bb1e0b82b188cd1b46b0d56d9dd7ac9865a
Philippe Luchansky <philuchansky@gmail.com>
2018-03-15 09:15:44 -0700

    User model starter code

--- models/User.js
+++ models/User.js
@@ -1,1 +1,9 @@
-//user model
+const
+  mongoose = require('mongoose'),
+  userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+  })
+
+const User = mongoose.model('User', userSchema)
